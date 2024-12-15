7b5090f75317665d4709c4030fe1bcb8fc5a5186
carlosilva10260 <121122527+carlosilva10260@users.noreply.github.com>
2024-12-15 16:57:21 +0000

    target  found !!!!

--- Assets/OnCollision.cs
+++ Assets/OnCollision.cs
@@ -5,21 +5,28 @@ public class MakeInvisibleOnCollision : MonoBehaviour
 {
     private Renderer objectRenderer; // For controlling visibility
     private Collider objectCollider; // For disabling collision
+    private Canvas canvas;
 
     void Start()
     {
         File.AppendAllLines(Path.Combine(Application.persistentDataPath, gameObject.name + "animal.csv"), new List<string>() { $"Frame\tAnimalName"});
         objectRenderer = GetComponent<Renderer>();
         objectCollider = GetComponent<Collider>();
+        GameObject canvasObject = GameObject.FindGameObjectWithTag("Notification");
+        canvas = canvasObject.GetComponent<Canvas>();
     }
 
-     void OnCollisionEnter(Collision collision)
+
+
+    void OnCollisionEnter(Collision collision)
     {
 
         // Make B invisible
         if (objectRenderer != null)
         {
             objectRenderer.enabled = false; // Hides the object
+            canvas.enabled = true;
+            
         }
 
         // Remove B's collider
